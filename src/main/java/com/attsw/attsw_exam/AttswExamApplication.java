7cdb96192973880eae588473adabd694081ab8a1
nuwan_r <nuwan_r@epiclanka.net>
2021-06-16 15:12:10 +0530

    inital commit project configurations

---
+++ src/main/java/com/attsw/attsw_exam/AttswExamApplication.java
@@ -0,0 +1,13 @@
+package com.attsw.attsw_exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttswExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttswExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/attsw/attsw_exam/AttswExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attsw.attsw_exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttswExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
